c343e0450637b8d7937a43f9c4530396e879711f
Lucas Eduardo Santos da Silva <leduardo.santos.1997@gmail.com>
2024-01-14 03:39:23 +0000

    feat: adjust eslint config

--- node.js
+++ node.js
@@ -3,47 +3,29 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["standard", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-  parser: "@typescript-eslint/parser",
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "eslint-plugin-import-helpers"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         printWidth: 80,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
+        trailingComma: 'all',
+        arrowParens: 'always',
         semi: false,
-      },
-    ],
-    "import-helpers/order-imports": [
-      "warn",
-      {
-        newlinesBetween: "always",
-        groups: ["module", ["parent", "sibling", "index"]],
-        alphabetize: { order: "asc", ignoreCase: true },
-      },
-    ],
-    "newline-before-return": "error",
-    "newline-after-var": "error",
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "interface",
-        format: ["PascalCase"],
-        prefix: ["I"],
       },
     ],
   },
   settings: {
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
   },
-};
+}
--- react.js
+++ react.js
@@ -2,81 +2,64 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    jest: true,
   },
   extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "standard",
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
     'plugin:@typescript-eslint/recommended',
-    "plugin:prettier/recommended",
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   plugins: [
-    "react",
-    "jsx-a11y",
-    "@typescript-eslint",
-    "eslint-plugin-import-helpers",
+    'react',
+    'jsx-a11y',
+    '@typescript-eslint'
   ],
   rules: {
-    "prettier/prettier": [
-      "error",
+    "react/self-closing-comp": "error",
+    'prettier/prettier': ["error", {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': false,
+      'endOfLine': 'auto',
+    }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
       {
-        printWidth: 80,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
-        semi: false,
-        endOfLine: 'auto'
+        elements: ['img'],
+        img: ['Image'],
       },
     ],
-    "import-helpers/order-imports": [
-      "warn",
-      {
-        newlinesBetween: "always",
-        groups: ["module", ["parent", "sibling", "index"]],
-        alphabetize: { order: "asc", ignoreCase: true },
-      },
-    ],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "jsx-a11y/alt-text": [
-      "warn",
-      {
-        elements: ["img"],
-        img: ["Image"],
-      },
-    ],
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-    "newline-before-return": "error",
-    "newline-after-var": "error",
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "interface",
-        format: ["PascalCase"],
-        prefix: ["I"],
-      },
-    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/no-unknown-property': 'error',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
   },
-};
+  ignorePatterns: [
+    'node_modules'
+  ]
+}
